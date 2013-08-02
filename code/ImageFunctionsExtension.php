8dd90082a8bcb05548f31f16b7f9be1849c3b963
Marcus Dalgren <marcus.dalgren@overlay.se>
2013-08-02 16:26:55 +0200

    Change image function

--- code/ImageFunctionsExtension.php
+++ code/ImageFunctionsExtension.php
@@ -3,7 +3,7 @@ class ImageFunctionsExtension extends DataExtension {
 	
 	public function RatioCrop($width, $height) {
 		$image = $this->owner->getFormattedImage('SetRatioSize', $width, $height);
-		$image = $this->owner->getFormattedImage('CroppedImage', $width, $height);
+		$image = $this->owner->getFormattedImage('CroppedFocusedImage', $width, $height);
 		return $image;
 	}
 }
